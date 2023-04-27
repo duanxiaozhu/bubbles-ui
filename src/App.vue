3c4e269f089bfa3f5b6d03e61a5cc7960c2fc1a7
zhuolin <zhuolin1014@163.com>
2023-04-27 19:15:22 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hi</div>
+  <shanzhu/>
+</template>
+
+<script>
+import shanzhu from './components/shanzhu.vue'
+
+export default {
+  name: 'App',
+  components: {
+    shanzhu
+  }
+}
+</script>
